e314ce84b65b066433fa804d56b9dc8743ca0015
satou555 <67263354+satou555@users.noreply.github.com>
2020-07-08 19:14:48 +0900

    修正
    メニュー登録機能追加

--- store_resister.php
+++ store_resister.php
@@ -3,25 +3,80 @@
 <head>
     <title>店舗情報登録</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="store_resister.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Anton">
+
 </head>
 <body>
-    <div class="big_form">
+    <div class="header">
+        <div class="header_wrap">
+            <div class="header_logo">
+                <a class="header_logoBtn" href="/">Takeout Kitchen</a>
+            </div>
+            <div class="header_menberBtn">
+                <div class="header_menberBtnItem">
+                    <a class="header_menberBtnLink" href="index_resisterTakeoutKitchen.php" rel="nofollow">新規登録</a>
+                </div>
+                <div class="header_menberBtnItem">
+                    <a class="header_menberLoginLink" href="index_loginTakeoutKitchen.php" rel="nofollow">ログイン</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container_bigbox">
+        <div class="panel_head">
+            <h1>店舗情報入力</h1>
+        </div>
         <div class="panel_body">
             <form action="store_resisterConf.php" method="POST">
                 <div class="form_content">
                     <p>店名</p>
-                    <input type="text" name="storename">
+                    <input class="form_input" type="text" name="storename">
                 </div>
                 <div class="form_content">
                     <p>住所</p>
-                    <input type="text" name="storeaddress">
+                    <input class="form_input" type="text" name="storeaddress">
                 </div>
                 <div class="form_content">
                     <p>電話番号</p>
-                    <input type="tel" name="storetel">
+                    <input class="form_input" type="tel" name="storetel">
+                </div>
+                <div>
+                    <p>メールアドレス　※店舗詳細には公開されません。</p>
+                    <input class="form_input" type="email" name="storeemail">
+                </div>
+                <div class="form_content">
+                    <div class="form_content_menu">
+                        <div class="menu">
+                            <p>品名</p>
+                            <input class="form_input_menu" type="text" name="menuname1">
+                            <input class="form_input_menu" type="text" name="menuname2">
+                            <input class="form_input_menu" type="text" name="menuname3">
+                            <input class="form_input_menu" type="text" name="menuname4">
+                            <input class="form_input_menu" type="text" name="menuname5">
+                            <input class="form_input_menu" type="text" name="menuname6">
+                            <input class="form_input_menu" type="text" name="menuname7">
+                            <input class="form_input_menu" type="text" name="menuname8">
+                            <input class="form_input_menu" type="text" name="menuname9">
+                            <input class="form_input_menu" type="text" name="menuname10">
+                        </div>
+                        <div class="menu">
+                            <p>価格</p>
+                            <input class="form_input_price" type="text" name="menuprice1">
+                            <input class="form_input_price" type="text" name="menuprice2">
+                            <input class="form_input_price" type="text" name="menuprice3">
+                            <input class="form_input_price" type="text" name="menuprice4">
+                            <input class="form_input_price" type="text" name="menuprice5">
+                            <input class="form_input_price" type="text" name="menuprice6">
+                            <input class="form_input_price" type="text" name="menuprice7">
+                            <input class="form_input_price" type="text" name="menuprice8">
+                            <input class="form_input_price" type="text" name="menuprice9">
+                            <input class="form_input_price" type="text" name="menuprice10">
+                        </div>
+                    </div>
                 </div>
                 <div class="submit_">
-                    <input type="submit" value="確認する">
+                    <input class="form_submit" type="submit" value="確認する">
                 </div>
             </form>
         </div>
--- store_resisterConf.php
+++ store_resisterConf.php
@@ -6,6 +6,64 @@ session_start();
 $storename=$_POST["storename"];
 $storeaddress=$_POST["storeaddress"];
 $storetel=$_POST["storetel"];
+$storeemail=$_POST["storeemail"];
+
+$menuname1=$_POST["menuname1"];
+$menuname2=$_POST["menuname2"];
+$menuname3=$_POST["menuname3"];
+$menuname4=$_POST["menuname4"];
+$menuname5=$_POST["menuname5"];
+$menuname6=$_POST["menuname6"];
+$menuname7=$_POST["menuname7"];
+$menuname8=$_POST["menuname8"];
+$menuname9=$_POST["menuname9"];
+$menuname10=$_POST["menuname10"];
+
+$menuprice1=$_POST["menuprice1"];
+$menuprice2=$_POST["menuprice2"];
+$menuprice3=$_POST["menuprice3"];
+$menuprice4=$_POST["menuprice4"];
+$menuprice5=$_POST["menuprice5"];
+$menuprice6=$_POST["menuprice6"];
+$menuprice7=$_POST["menuprice7"];
+$menuprice8=$_POST["menuprice8"];
+$menuprice9=$_POST["menuprice9"];
+$menuprice10=$_POST["menuprice10"];
+
+$menunames=array($menuname1,$menuname2,$menuname3,$menuname4,$menuname5,
+                $menuname6,$menuname7,$menuname8,$menuname9,$menuname10);
+
+$menuprices=array($menuprice1,$menuprice2,$menuprice3,$menuprice4,$menuprice5,
+                $menuprice6,$menuprice7,$menuprice8,$menuprice9,$menuprice10);                
+
+/*登録するボタンを押したら情報をデータベースに登録する*/
+if(isset($_POST["submit_"])){
+    /*データベース接続*/ 
+    $dsn="mysql:dbname=".DB_NAME.";host=".DB_HOST.";charset=utf8";
+    $pdo= new PDO($dsn,DB_USER,DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_SILENT);
+       
+    /*データ挿入*/
+    $stmt=$pdo->prepare("INSERT INTO kitchens (storename,addres,tel,mail) VALUE(?,?,?,?)");
+    $stmt->bindValue(1,$_POST["storename"]);
+    $stmt->bindValue(2,$_POST["storeaddress"]);
+    $stmt->bindValue(3,$_POST["storetel"]);
+    $stmt->bindValue(4,$_POST["storeemail"]);
+    $stmt->execute();
+
+    $stmt=$pdo->prepare("INSERT INTO menu (product,price) VALUE(?,?)");
+    foreach($menunames as $menuname){
+        $stmt->bindValue(1,$menuname);
+    }
+    foreach($menuprices as $menuprice){
+        $stmt->bindValue(2,$menuprice);
+    }
+    $stmt->execute();
+    
+
+    header("Location:storeresister_success.php");
+    exit;
+}
 ?>
 
 <!doctype html>
@@ -26,14 +84,59 @@ $storetel=$_POST["storetel"];
             <div>
                 <p>店舗名</p>
                 <p><?php echo $storename ?></p>
+                <input type="hidden" name="storename" value="<?php echo $storename ?>"> 
             </div>
             <div>
                 <p>住所</p>
                 <p><?php echo $storeaddress ?></p>
+                <input type="hidden" name="storeaddress" value="<?php echo $storeaddress ?>"> 
             </div>
             <div>
                 <p>電話番号</p>
                 <p><?php echo $storetel ?></p>
+                <input type="hidden" name="storetel" value="<?php echo $storetel ?>"> 
+            </div>
+            <div>
+                <p>メールアドレス</p>
+                <p><?php echo $storeemail ?></p>
+                <input type="hidden" name="storeemail" value="<?php echo $storeemail ?>"> 
+            </div>
+            <div>
+                <p>メニュー</p>
+
+                <p>品名</p>
+                <p><?php 
+                foreach($menunames as $menuname){
+                    echo $menuname;
+                } ?></p>
+                <input type="hidden" name="menuname1" value="<?php echo $menuname1 ?>">
+                <input type="hidden" name="menuname2" value="<?php echo $menuname2 ?>">
+                <input type="hidden" name="menuname3" value="<?php echo $menuname3 ?>">
+                <input type="hidden" name="menuname4" value="<?php echo $menuname4 ?>">
+                <input type="hidden" name="menuname5" value="<?php echo $menuname5 ?>">
+                <input type="hidden" name="menuname6" value="<?php echo $menuname6 ?>">
+                <input type="hidden" name="menuname7" value="<?php echo $menuname7 ?>">
+                <input type="hidden" name="menuname8" value="<?php echo $menuname8 ?>">
+                <input type="hidden" name="menuname9" value="<?php echo $menuname9 ?>">
+                <input type="hidden" name="menuname10" value="<?php echo $menuname10 ?>">
+
+                <p>価格</p>
+                <p><?php 
+                foreach($menuprices as $menuprice){
+                    echo $menuprice;
+                }
+                ?></p>
+                <input type="hidden" name="menuprice1" value="<?php echo $menuprice1 ?>"> 
+                <input type="hidden" name="menuprice2" value="<?php echo $menuprice2 ?>"> 
+                <input type="hidden" name="menuprice3" value="<?php echo $menuprice3 ?>"> 
+                <input type="hidden" name="menuprice4" value="<?php echo $menuprice4 ?>"> 
+                <input type="hidden" name="menuprice5" value="<?php echo $menuprice5 ?>"> 
+                <input type="hidden" name="menuprice6" value="<?php echo $menuprice6 ?>"> 
+                <input type="hidden" name="menuprice7" value="<?php echo $menuprice7 ?>"> 
+                <input type="hidden" name="menuprice8" value="<?php echo $menuprice8 ?>"> 
+                <input type="hidden" name="menuprice9" value="<?php echo $menuprice9 ?>"> 
+                <input type="hidden" name="menuprice10" value="<?php echo $menuprice10 ?>"> 
+
             </div>
             <div>
                 <input type="submit" name="submit_" value="登録する">
@@ -41,23 +144,3 @@ $storetel=$_POST["storetel"];
         </form>
 </body>
 </html>
-
-<?php
-/*POSTで送信されている */
-if(isset($_POST["submit"])){
-    /*データベース接続*/ 
-    $dsn="mysql:dbname=".DB_NAME.";host=".DB_HOST.";charset=utf8";
-    $pdo= new PDO($dsn,DB_USER,DB_PASS);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_SILENT);
-       
-    /*データ挿入*/
-    $stmt=$pdo->prepare("INSERT INTO kitchens (storename,addres,tel) VALUE(?,?,?)");
-    $stmt->bindValue(1,$storename);
-    $stmt->bindValue(2,$storeaddress);
-    $stmt->bindValue(3,$storetel);
-    $stmt->execute();
-    $_SESSION["resister_message"]="登録が完了しました";
-    header("Location:storeresister_success.php");
-    exit;
-}
-?>
